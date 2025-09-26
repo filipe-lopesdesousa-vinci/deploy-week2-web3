96a85d6d0f429dc8fa85ca1ad601467d1c69da9d
Filipe Lopes de Sousa <filipe.lopesdesousa@student.vinci.be>
2025-09-26 11:23:22 +0200

    db test 2

--- backend/db-populate.js
+++ backend/db-populate.js
@@ -3,6 +3,7 @@ const { PrismaClient } = require('./generated/prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
+    console.log('Seeding database...');
   const expenses = await prisma.expense.createMany({
     data: [
       { date: '2025-01-16T00:00:00Z', description: 'Example expense #1 from Alice', payer: 'Alice', amount: 25.5 },
@@ -10,11 +11,14 @@ async function main() {
       { date: '2025-01-15T00:00:00Z', description: 'Example expense #3 from Alice', payer: 'Alice', amount: 2 },
     ],
   });
+  console.log('after expenses')
 }
 
 main()
   .finally(async () => {
+    console.log('Seeding finished.');
     await prisma.$disconnect();
+    console.log('Disconnected from database.');
   })
   .catch(async (e) => {
     console.error(e);
